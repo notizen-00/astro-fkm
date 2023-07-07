3cffd16e3e1f8f5638951d8de11755c419075df8
dipta ikromi m <dipta.ikromi@unej.ac.id>
2023-07-07 08:00:39 +0700

    integration astro build new

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,19 @@ import tailwind from "@astrojs/tailwind";
 import Unfonts from 'unplugin-fonts/astro';
 import { Editor } from '@tinymce/tinymce-react';
 import react from "@astrojs/react";
+import robotsTxt from "astro-robots-txt";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site:'https://fkm.unej.ac.id',
   integrations: [tailwind(), Unfonts({
     google: {
       families: ['Inter']
     }
-  }), 
-  react()]
+  }), react(), robotsTxt(), partytown(), sitemap()]
 });
